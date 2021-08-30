fceb71daf13b63ab762d243ba002334fbe0255d2
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2021-08-30 22:05:06 +0200

    API

---
+++ API/Data/SqlDbContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using API.Entities;
+
+#nullable disable
+
+namespace API.Data
+{
+    public partial class SqlDbContext : DbContext
+    {
+        public SqlDbContext()
+        {
+        }
+
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasColumnName("Order_Id");
+            });
+
+            modelBuilder.Entity<OrderProduct>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ItemId })
+                    .HasName("PK__Order_Pr__56C3884398C12BA9");
+
+                entity.ToTable("Order_Product");
+
+                entity.Property(e => e.OrderId).HasColumnName("Order_Id");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderProducts)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Order_Pro__Order__60A75C0F");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderProducts)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Order_Pro__Produ__619B8048");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Color)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ShortDescription)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.Size)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
